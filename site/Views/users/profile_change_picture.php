e811ffda43f6b8dc388b81e96f97e89a439cdcf0
Todor Vasilev <bggadbg@abv.bg>
2016-04-27 15:33:08 +0300

    styled profile_change_picture.php

--- site/Views/users/profile_change_picture.php
+++ site/Views/users/profile_change_picture.php
@@ -1,58 +1,71 @@
-<div class="change-profile">
-	<header>
-		<h2>Change profile picture</h2>
-	</header>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Change Profile Picture</title>
 
-	<table class="upload-picture">
-		<th>
-			<tr>
-				<button type="button" class="btn btn-default btn-lg">
-  					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Upload Picture
-				</button>
-			</tr>
-			<tr>
-				<button type="button" class="btn btn-default btn-lg">
-  					<span class="glyphicon glyphicon-camera" aria-hidden="true"></span> Take a Picture
-				</button>
-			</tr>
-		</th>
-	</table>
+	<link rel="stylesheet" type="text/css" href="../style/reset.css">
+	<link rel="stylesheet" type="text/css" href="../style/bootstrap-style.css">
+	<link rel="stylesheet" type="text/css" href="../style/default.css">
+	<link rel="stylesheet" type="text/css" href="../style/change-picture.css">
+</head>
+<body>
+	<div class="change-profile">
+		<header>
+			<h2>Change profile picture</h2>
+		</header>
 
-	<div class="my-pictures">
-		<h3>My Pictures</h3>
-		<table>
-			<tbody>
+		<table class="upload-picture">
+			<th>
 				<tr>
-					<td>
-						<a href="#" rel="makeprofile">
-							<div class="scaled-image">
-								<img class="image" src="#" alt="I am handsome"></img>
-							</div>
-						</a>
-					</td>
-					<td>
-						<a href="#" rel="makeprofile">
-							<div class="scaled-image">
-								<img class="image" src="#" alt="I am handsome"></img>
-							</div>
-						</a>
-					</td>
-					<td>
-						<a href="#" rel="makeprofile">
-							<div class="scaled-image">
-								<img class="image" src="#" alt="I am handsome"></img>
-							</div>
-						</a>
-					</td>
-					<td>
-						<a href="#" rel="makeprofile">
-							<div class="scaled-image">
-								<img class="image" src="#" alt="I am handsome"></img>
-							</div>
-						</a>
-					</td>
+					<button type="button" class="btn btn-default btn-lg">
+	  					<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Upload Picture
+					</button>
 				</tr>
-			</tbody>
+				<tr>
+					<button type="button" class="btn btn-default btn-lg">
+	  					<span class="glyphicon glyphicon-camera" aria-hidden="true"></span> Take a Picture
+					</button>
+				</tr>
+			</th>
 		</table>
+
+		<div class="my-pictures">
+			<h3>My Pictures</h3>
+			<table>
+				<tbody>
+					<tr>
+						<td>
+							<a href="#" rel="makeprofile">
+								<div class="scaled-image">
+									<img class="image" src="#" alt="I am handsome"></img>
+								</div>
+							</a>
+						</td>
+						<td>
+							<a href="#" rel="makeprofile">
+								<div class="scaled-image">
+									<img class="image" src="#" alt="I am handsome"></img>
+								</div>
+							</a>
+						</td>
+						<td>
+							<a href="#" rel="makeprofile">
+								<div class="scaled-image">
+									<img class="image" src="#" alt="I am handsome"></img>
+								</div>
+							</a>
+						</td>
+						<td>
+							<a href="#" rel="makeprofile">
+								<div class="scaled-image">
+									<img class="image" src="#" alt="I am handsome"></img>
+								</div>
+							</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
+</body>
+</html>
--- site/Views/users/profile_crop_picture.php
+++ site/Views/users/profile_crop_picture.php
@@ -2,7 +2,7 @@
 <html lang="en" >
     <head>
         <meta charset="utf-8" />
-        <title>HTML5 image crop tool | Script Tutorials</title>
+        <title>Crop profile picture</title>
         <link href="css/main.css" rel="stylesheet" type="text/css" />
 
         <script src="http://code.jquery.com/jquery-latest.min.js"></script>
@@ -11,7 +11,6 @@
     <body>
         <header>
             <h2>HTML5 image crop tool</h2>
-            <a href="https://www.script-tutorials.com/html5-image-crop-tool/" class="stuts">Back to original tutorial on <span>Script Tutorials</span></a>
         </header>
 
         <div class="container">
@@ -19,7 +18,8 @@
                 <button onclick="getResults()">Crop</button>
             </div>
 
-            <canvas id="panel" width="779" height="519"></canvas>
+            <canvas id="panel" width="779" height="519">
+            </canvas>
 
             <div id="results">
                 <h2>Please make selection for cropping and click 'Crop' button.</h2>
