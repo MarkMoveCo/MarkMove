8283a983e90c8f46f1d64665eadfbbff15baf43d
Todor Vasilev <bggadbg@abv.bg>
2016-05-16 18:52:08 +0300

    sidebar from home.php is moved to the right; updates to create.css

--- site/Views/publications/create.php
+++ site/Views/publications/create.php
@@ -13,6 +13,7 @@ namespace site\Views\publications;
   <link rel="stylesheet" type="text/css" href="../style/header-footer-style.css">
   <link rel="stylesheet" type="text/css" href="../style/default.css">
   <link rel="stylesheet" type="text/css" href="../style/create.css">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -36,46 +37,61 @@ namespace site\Views\publications;
       <div id="sidebar-wrapper">
           <ul class="sidebar-nav">
                 <li class="sidebar-brand">
-                  <i class="fa fa-cog"> New draft</i>
+                  <i class="fa fa-cog"></i>New draft
                 </li>
+
                 <li>
                   <button class="btn preview-button">Preview</button>
-                  <button class="btn publish-button">Publish <i id="postpone" class="fa fa-calendar"></i></button>
+                  <button class="btn publish-button">Publish</button>
+                </li>
+
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-newspaper-o"></i>Categories and Tags</a>
                 </li>
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa  fa-hashtag"></i>Categories and Tags</a>
-                <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-image"></i>Featured Images</a>
-                <li>
+                </li>
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa  fa-share-alt"></i>Sharing</a>
-                <li>
+                </li>
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa  fa-wrench"></i>More options</a>
-                <li>
+                </li>
           </ul>
       </div>
 
       <div class="container">
         <div class="content">
+          <div class="element">
+            <form role="form" action="" method="post" id="create-form">
+              <div class="form-group">
+                  <input class="form-control" type="text" name="title" placeholder="Title"></input> 
+              </div>
+            </form>
+          </div>
+
+          <div class="element">
+            <form role="form" action="" method="post" id="create-form">
+              <div class="form-group">
+                  <input class="form-control" type="text" name="author" placeholder="Author"></input> 
+              </div>
+            </form>
+          </div>
+          
           <ul class="nav nav-tabs">
               <li class="active"><a href="#html" data-toggle="tab">HTML</a></li>
               <li><a data-toggle="tab" href="#visual">Visual</a></li>
           </ul>
+
           <div class="tab-content">
             <div class="tab-pane active" id="html">
-              <textarea class="form-control" name="raw-HTML" rows="10" form="create-form"></textarea>
+              <textarea class="form-control" name="raw-HTML" rows="12" form="create-form"></textarea>
             </div>
             <div class="tab-pane" id="visual">
               
             </div>
           </div>
-          <form role="form" action="" method="post" id="create-form">
-            <div class="form-group">
-                <label for="text">Name:</label>
-                <input class="form-control" type="text" name="title"></input> 
-            </div>
-          </form>
+
         </div>
       </div> <!-- container -->
     </main>
