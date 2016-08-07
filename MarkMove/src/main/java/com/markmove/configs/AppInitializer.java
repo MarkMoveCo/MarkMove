3bddf18873c519b4284a30a24c3bda67d1246a45
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-08 00:21:10 +0300

    Made the Configuration with Java classes

---
+++ MarkMove/src/main/java/com/markmove/configs/AppInitializer.java
@@ -0,0 +1,20 @@
+package com.markmove.configs;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
