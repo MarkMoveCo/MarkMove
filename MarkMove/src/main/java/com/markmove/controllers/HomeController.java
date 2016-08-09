3faf8396ce9aab0067423d3cae8f3c67fc3a787f
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-09 12:21:23 +0300

    Added Spring Security

--- MarkMove/src/main/java/com/markmove/controllers/HomeController.java
+++ MarkMove/src/main/java/com/markmove/controllers/HomeController.java
@@ -3,12 +3,20 @@ package com.markmove.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Model model){
         model.addAttribute("message", "Hello Bitches!");
         return "index";
+    }
+
+    @RequestMapping("/landmarks")
+    public String landmarks(Model model){
+        model.addAttribute("user", "Hashim");
+        return "landmarks";
     }
 }
