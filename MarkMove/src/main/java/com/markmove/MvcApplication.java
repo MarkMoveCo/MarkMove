63af34c88a3a6b26c71f4f3560d9fee86ff35207
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-26 18:24:21 +0300

    Removed unnecessary Configurations, target package, and fixed layouts

--- MarkMove/src/main/java/com/markmove/MvcApplication.java
+++ MarkMove/src/main/java/com/markmove/MvcApplication.java
@@ -2,15 +2,9 @@ package com.markmove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MvcApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MvcApplication.class);
-    }
+public class MvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MvcApplication.class,args);
