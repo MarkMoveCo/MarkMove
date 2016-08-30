6b2ae37dce7108b7e34a302a9053d3357bf501eb
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-30 16:06:04 +0300

    CHanged extending interface so that can easily use pagining functionality

--- MarkMove/src/main/java/com/markmove/repositories/LandmarkRepository.java
+++ MarkMove/src/main/java/com/markmove/repositories/LandmarkRepository.java
@@ -2,8 +2,10 @@ package com.markmove.repositories;
 
 import com.markmove.models.Landmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LandmarkRepository extends JpaRepository<Landmark, Long>{
+public interface LandmarkRepository extends PagingAndSortingRepository<Landmark, Long> {
 }
