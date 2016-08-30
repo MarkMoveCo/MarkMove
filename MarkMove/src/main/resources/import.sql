7febe73bfec162ee0524c7926d2bbd4ced125d20
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-30 16:01:10 +0300

    Removed ROlE pefixes

--- MarkMove/src/main/resources/import.sql
+++ MarkMove/src/main/resources/import.sql
@@ -4,9 +4,9 @@ INSERT INTO user (age, email, gender, password_hash, username) VALUES(19, 'hasim
 INSERT INTO user (age, email, gender, password_hash, username) VALUES(20, 'toshko.vasilev@yahoo.com', 'Male', '$2a$10$.ileK4xLhSZ7rBRdtVMq3OSn2t3ZLCiuba7yt06rC9arKquY8tzbm', 'Todor');
 /*Username is Todor with PASSWORD = 1111; (four 1-s)!*/
 
-INSERT INTO role (name) VALUES('ROLE_USER');
-INSERT INTO role (name) VALUES('ROLE_PUBLISHER');
-INSERT INTO role (name) VALUES('ROLE_ADMIN');
+INSERT INTO role (name) VALUES('USER');
+INSERT INTO role (name) VALUES('PUBLISHER');
+INSERT INTO role (name) VALUES('ADMIN');
 
 INSERT INTO user_role (user_id, role_id) values (1,3);
 INSERT INTO user_role (user_id, role_id) values (2,3);
