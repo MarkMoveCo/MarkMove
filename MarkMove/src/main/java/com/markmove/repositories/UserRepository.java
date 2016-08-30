464e37bdc9ea1793ad3e06e7d9ba5a6f3beebdb5
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-30 20:31:04 +0300

    Added custom methods

--- MarkMove/src/main/java/com/markmove/repositories/UserRepository.java
+++ MarkMove/src/main/java/com/markmove/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import com.markmove.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    List<User> findAllByOrderByUsernameAsc();
 }
