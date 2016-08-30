92ffb149e06dd8605ffdfe9f4a396f4c54826307
hasimsolakov <hasimkadrinov@gmail.com>
2016-08-30 20:30:29 +0300

    Added show edit User permission function

--- MarkMove/src/main/java/com/markmove/controllers/UserController.java
+++ MarkMove/src/main/java/com/markmove/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.markmove.controllers;
 
+import com.markmove.models.Role;
 import com.markmove.models.User;
 import com.markmove.services.PictureService;
+import com.markmove.services.RoleService;
 import com.markmove.services.SystemNotificationService;
 import com.markmove.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -22,6 +26,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
 
     @Autowired
     private SystemNotificationService systemNotificationService;
@@ -49,5 +56,14 @@ public class UserController {
         User currentUser = this.userService.findByUsername(principal.getName());
         this.pictureService.create(file, currentUser, false);
         return "redirect:/";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/users/permissions")
+    public String editUsersPermissions(Model model) {
+        Iterable<User> users = this.userService.findAllOrderedByUsername();
+        Iterable<Role> roles = this.roleService.findAllOrderedById();
+        model.addAttribute("users", users);
+        model.addAttribute("roles", roles);
+        return "users/permissions";
     }
 }
